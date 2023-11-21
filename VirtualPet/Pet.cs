fa73c8f0eb1856ffdf134da55dffd85e45c38b93
Sonsim <141270918+Sonsim@users.noreply.github.com>
2023-11-21 14:22:14 +0100

    Add project files.

---
+++ VirtualPet/Pet.cs
@@ -0,0 +1,109 @@
+﻿namespace VirtualPet
+{
+    public class Pet
+    {
+
+        public readonly string Name;
+        public readonly int Age;
+        private int _fun;
+        private int _hunger;
+        private int _toilet;
+
+        public Pet(string name, int age, int Fun = 10, int Hunger = 10, int Toilet = 10)
+        {
+            Name = name;
+            Age = age;
+            _fun = Fun;
+            _hunger = Hunger;
+            _toilet = Toilet;
+        }
+
+        public void getInformation()
+        {
+            Console.WriteLine(""+Name);
+            Console.WriteLine($"Alder: {Age}");
+            Console.WriteLine($"{getProgress("Hunger", _hunger)}");
+            Console.WriteLine("   " + getProgress("Fun", _fun));
+            Console.WriteLine($"{getProgress("Toilet", _toilet)}");
+        }
+
+        private string getProgress(string Type, int current)
+        {
+            int _blank = 10 - current;
+            string _string = "";
+
+            _string = $"{Type}: [";
+            for (int i = 0; i < current; i++)
+            {
+                _string += "■";
+            }
+            for (int i = 0; i < _blank; i++)
+            {
+                _string += " ";
+            }
+            _string += "]";
+
+            return _string;
+        }
+
+        public void PlayWith()
+        {
+            _fun+= 5;
+            _hunger--;
+            _toilet--;
+            fixStatus();
+            Console.WriteLine($"{Name} har det gøy!");
+        }
+
+        public void Feed()
+        {
+            _fun--;
+            _hunger += 5;
+            _toilet--;
+            fixStatus();
+            Console.WriteLine($"{Name} liker maten og føler seg mettere");
+        }
+
+        public void GoToilet()
+        {
+            _fun--;
+            _hunger--;
+            _toilet += 5;
+            fixStatus();
+            Console.WriteLine("*PLUMP*");
+        }
+
+        public void Idle()
+        {
+            _fun--;
+            _hunger--;
+            _toilet--;
+            fixStatus();
+        }
+
+        private void fixStatus()
+        {
+            if (_fun <= 3)
+                Console.WriteLine("jeg kjeeeeeeeeeeeeeder meggg....");
+            if (_hunger <= 3)
+                Console.WriteLine("jeg døøøøøøøøøøør....");
+            if (_toilet == 0)
+                Console.WriteLine("jeg bæsja i senga....");
+
+            if (_fun < 0)
+                _fun = 0;
+            if (_hunger < 0)
+                _fun = 0;
+            if (_toilet < 0)
+                _toilet = 0;
+
+            if (_fun > 10)
+                _fun = 10;
+            if (_hunger > 10)
+                _hunger = 10;
+            if (_toilet > 10)
+                _toilet = 10;
+        }
+
+    }
+}
---
+++ VirtualPet/Program.cs
@@ -0,0 +1,70 @@
+﻿namespace VirtualPet
+{
+    public class Program
+    {
+        static Pet CurrentPet;
+        static List<Pet> _pets = new List<Pet>
+            {
+                new Pet("Calo", 6),
+                new Pet("Easy", 3),
+                new Pet ("Atlas", 5),
+            };
+
+        static void Main()
+        {
+
+            SelectPet();
+        }
+
+        static void SelectPet()
+        {
+           
+            Console.WriteLine("Which pet do you want to select?");
+            for (int i = 0; i < _pets.Count; i++)
+            {
+                Console.WriteLine($"[{i + 1}] : {_pets[i].Name}");
+            }
+
+            int ans = Convert.ToInt32(Console.ReadLine());
+            CurrentPet = _pets[ans - 1];
+
+            ShowPet();
+        }
+        static void ShowPet()
+        {
+            CurrentPet.getInformation();
+            Console.WriteLine("Hva vil du gjøre?");
+            Console.WriteLine($"[0] - Ta vare på annet dyr");
+            Console.WriteLine($"[1] - Gi mat til {CurrentPet.Name}");
+            Console.WriteLine($"[2] - Leke med {CurrentPet.Name}");
+            Console.WriteLine($"[3] - Gå ut med {CurrentPet.Name}");
+            Console.WriteLine("----------------------------------");
+            int ans = Convert.ToInt32(Console.ReadLine());
+            Console.Clear();
+
+            switch  (ans)
+            {
+                case 0:
+                    CurrentPet = null;
+                    SelectPet();
+                    return;
+                case 1:
+                    CurrentPet.Feed();
+                    ShowPet();
+                    return;
+                case 2:
+                    CurrentPet.PlayWith();
+                    ShowPet();
+                    return;
+                case 3:
+                    CurrentPet.GoToilet();
+                    ShowPet();
+                    return;
+                default:
+                    CurrentPet.Idle();
+                    ShowPet();
+                    return;
+            }
+        }
+    }
+}
